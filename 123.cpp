0fabdf2a3a2bed196dbed42fcc224ee612fc0335
arim18 <50289468+arim18@users.noreply.github.com>
2020-03-03 13:04:42 +0300

    Add files via upload

---
+++ 123.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+	
+	int k=1;
+
+for(int i=1;i<6;i++){
+	k++;
+	printf("(%d)",i);
+	for(int j=1;j<k;j++){
+		printf("%d",j);
+	}
+printf("\n");
+}
+printf("*********\n");
+int l=6;
+for(int i=5;0<i;i--){
+	l--;
+	int m=0;
+	printf("(%d)",i);
+	for(int j=l;0<j;j--){
+		m++;
+		printf("%d",m);
+	}
+printf("\n");
+}
+printf("*********\n");
+
+
+
+return 0;
+}
